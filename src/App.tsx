f60163caba1b44e15078ce15bb8d7a7d0e8ca344
Paul Nwodo <paulnwodotochukwu180@gmail.com>
2025-10-16 17:13:20 +0100

    refactor: improve layout and styling in App, Hero, RolesSection, and SideBar components

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ function App() {
 
   return (
     <>
-      <div className="flex max-w-[687px bg-gray-400] h-[100vh]">
+      <div className="flex max-w-[687px h-auto">
         {/* SIDEBAR SECTION */}
-        <div className="flex sidebar w-[200px] bg-red-400 max-md:hidden z-10 max-md:flex-">
+        <div className="flex sidebar w-[200px] max-md:hidden z-10 max-md:flex-">
           <SideBar />
         </div>
         {/* BODY */}
-        <div className="body flex flex-col w-[100%] bg-green-400 items-center overflow-x-hidden">
+        <div className="body flex flex-col w-[100%] items-center overflow-x-hidden">
           {/* HEADER SECTION */}
           <div className="flex bg-gray-600 w-full h-[70px] items-center justify-center fixed top-0 mb-15">
             <Header />
           </div>
           {/* HERO SECTION */}
-          <div className="flex bg-green-600 w-full h-[100%] items-center justify-center">
+          <div className="flex w-full items-center justify-center">
             <Hero />
           </div>
           {/* FOOTER SECTION */}
-          <div className="flex bg-gray-600 w-full h-[70px] items-center justify-center">
+          <div className="flex bg-gray-600 w-full h-[70vh] items-center justify-center">
             <h1 className="text-3xl flex">Footer</h1>
           </div>
         </div>
