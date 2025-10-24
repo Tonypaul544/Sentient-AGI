3621ffae1151bfac83e52a89f6dbf82f3c3d2044
Paul Nwodo <paulnwodotochukwu180@gmail.com>
2025-10-24 11:18:40 +0100

    back to main, ahhh, i don suffer

--- src/roles.tsx
+++ src/roles.tsx
@@ -8,35 +8,35 @@ export default function RolesSection() {
 
   return(
     <>
-      <div className="flex flex-col w-[60%] h-auto p-5 m-auto items-center justify-center gap-7 max-md:w-[100%] mt-10 max-md:mt-15 pb-10">
+      <div className="flex flex-col w-[60%] h-auto p-5 m-auto items-center justify-center gap-7 max-md:w-[100%] mt-10 max-md:mt-[30%] pb-10">
         <div className="flex flex-col">
         <h1 className="text-white font-bold text-xl slide-up">SentientAGI Contribution roles</h1>
         </div>
 
-        <div className="flex max-md:flex-col w-[100%] h-full justify-center items-center gap-2 max-md:gap-1  transistion-all duration-500 ">
+        <div className="flex w-content max-md:w-[100%] flex-wrap h-full justify-center items-center gap-2 max-md:gap-4  transistion-all duration-500 ">
 
-          <div className="flex h-[80px] w-[120px] max-md:w-[90%] max-md:h-[120px] max-md:m-auto  max-md:mb-2 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:inner-shadow-xl hover:scale-[1.05] transistion-all duration-500">
+          <div className="flex h-[80px] w-[120px] max-md:min-w-[150px] max-md:min-h-[90px] max-md:max-w-[40%] max-md:flex-1 max-md:h-[120px] max-md:m-auto  max-md:mb-2 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:inner-shadow-xl hover:scale-[1.05] transistion-all duration-500 my-2">
             <div className="flex p-2 bg-glass-tick inner-shadow rounded-lg">
               <BsFillBuildingsFill />
             </div>
             <p className="text-white text-xs">Builder Role</p>
           </div>
           
-          <div className="flex h-[80px] w-[120px]  max-md:w-[90%] max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
+          <div className="flex h-[80px] w-[120px] max-md:min-w-[150px] max-md:min-h-[90px] max-md:max-w-[40%] max-md:flex-1 max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
             <div className="flex p-2 bg-glass-tick inner-shadow rounded-lg">
               <PiStudentFill />
             </div>
             <p className="text-white text-xs">Educator Role</p>
           </div>
 
-          <div className="flex h-[80px] w-[120px]  max-md:w-[90%] max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
+          <div className="flex h-[80px] w-[120px] max-md:min-w-[150px] max-md:min-h-[90px] max-md:max-w-[40%] max-md:flex-1 max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
             <div className="flex p-2 bg-glass-tick inner-shadow rounded-lg">
               <FaPaintRoller />
             </div>
             <p className="text-white text-xs">Artist Role</p>
           </div>
 
-          <div className="flex h-[80px] w-[120px]  max-md:w-[90%] max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
+          <div className="flex h-[80px] w-[120px] max-md:min-w-[150px] max-md:min-h-[90px] max-md:max-w-[40%] max-md:flex-1 max-md:h-[120px] m-auto  max-md:mb-2 bg-red-100 bg-glass rounded-xl flex-col items-center justify-center gap-2 inner-shadow-lg hover:scale-[1.05] transistion-all duration-500">
             <div className="flex p-2 bg-glass-tick inner-shadow rounded-lg">
               <GiGroundSprout />
             </div>
