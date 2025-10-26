e21beb68992b1d76356b9dd7ff7eaa86db8951ca
Paul Nwodo <paulnwodotochukwu180@gmail.com>
2025-10-26 12:32:55 +0100

    checking repo

--- src/header.tsx
+++ src/header.tsx
@@ -19,13 +19,13 @@ export default function Header() {
     <>
       <div className="flex items-center w-full justify-between px-5 bg-glass inner-shadow h-[100%] max-md:relative bg-transparent-sm">
         <div className="flex w-content h-content items-center justify-center">
-          <img src={Logo} alt="Sentient Logo" className="w-[150px] h-[35px] rounded-lg logo mr-1" />
+          <img src={Logo} alt="Sentient Logo" className="w-[165px] h-[45px] rounded-lg logo mr-1" />
           {/* <p className="text-[#fff] font-bold text-[23px] expand">SentientAGI</p> */}
         </div>
 
         <nav id="navBar" className="flex gap-4 max-sm:gap-3 max-sm:w-[100%]  h-auto mt-1 max-md:flex-col max-md:w-[500px]  max-md:absolute  max-md:items-center max-md:bg-[#222623] max-md:mx-auto max-md:gap-5 right-0 top-9  max-md:py-3 max-md:hidden mr-17" style={isOpen ? {display: 'none'} : {display: 'flex'}}>
 
-          <Link to="about" smooth duration={500} className="text-white text-sm expand font-bold px-5 py-1 rounded-md hover:bg-[#525653]  max-md:px-5  max-md:py-1  max-md:rounded-md  max-md:w-[200px]  max-md:text-center hover:inner-shadow hover:scale-[1.096] 
+          <Link to="about" smooth duration={500} className="text-white text-sm expand font-bold px-5 py-1 rounded-md hover:bg-[#525653]  max-md:px-5  max-md:py-1  max-md:rounded-md  max-md:w-[200px]  max-md:text-center hover:inner-shadow hover:scale-[1.096]
            active:bg-gray-500 transistion-all duration-300">About</Link>
 
           <Link to="about" smooth duration={500} className="text-white text-sm expand font-bold  px-5 py-1 rounded-md hover:bg-[#525653]  max-md:px-5  max-md:py-1  max-md:rounded-md  max-md:w-[200px]  max-md:text-center hover:scale-[1.096] 
